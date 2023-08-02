e9fea4d9f6995faf88e880a65de84aec7622c3f5
SungYeonJun <elwlahs60@gmail.com>
2023-08-03 00:25:22 +0900

    [UPDATE] Hackerton API

---
+++ hackerton-ntb/src/main/java/com/ntb/hackertonntb/HackertonNtbApplication.java
@@ -0,0 +1,13 @@
+package com.ntb.hackertonntb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackertonNtbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackertonNtbApplication.class, args);
+	}
+
+}
---
+++ hackerton-ntb/src/test/java/com/ntb/hackertonntb/HackertonNtbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntb.hackertonntb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackertonNtbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
